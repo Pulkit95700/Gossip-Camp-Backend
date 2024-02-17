8632a3f2c02095ecb68e344e3e091a00e9b0787f
Pulkit Gupta <pulkitgupta95700@gmail.com>
2024-02-17 23:52:21 +0530

    removed credentials equal to true

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    // credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
