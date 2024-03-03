7812b78c24073b6999bd53ca0594cd95d12fb691
Pulkit Gupta <pulkitgupta95700@gmail.com>
2024-03-03 14:08:24 +0530

    Add collegeName to profile response

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
